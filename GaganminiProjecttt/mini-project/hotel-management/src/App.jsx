74e4e0112d6c64150ff5e6eb9b903538638bb84b
GaganSingh12 <gagansinghji1618@gmail.com>
2025-07-14 15:07:40 +0530

    Web devolpment project

---
+++ GaganminiProjecttt/mini-project/hotel-management/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import SearchPage from './SearchPage';
+
+function App() {
+  return (
+    <div>
+      <SearchPage />
+    </div>
+  );
+}
+
+export default App;
+
+
+
+
